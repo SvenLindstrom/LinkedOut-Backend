83bd97b05edadf1f798f3f39d9e98f6ffc586eee
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-10-06 20:03:34 +0200

    db schema

---
+++ databases/schemas/schemas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+	id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+	user_id VARCHAR(50) NOT NULL,
+	name VARCHAR(50) NOT NULL,
+	connecting boolean DEFAULT false,
+	location geography(POINT, 4326)
+);
