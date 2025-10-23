df23ba5256d254292f0d015ca02561134cc72c29
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-10-23 15:04:16 +0200

    fix slight bugs

--- services/user/model.go
+++ services/user/model.go
@@ -15,21 +15,32 @@ func (m *UserModel) getInfo(id string) (UserInfo, error) {
 	var interestsJSON []byte
 
 	err := m.DB.QueryRow(
-		`SELECT id, name, bio, profession
-		json_agg(json_build_object('id', interests.id, 'name', interests.name)) AS interests
+		`SELECT
+		users.id,
+		users.name,
+		users.bio,
+		users.profession,
+		COALESCE(
+			json_agg(
+				json_build_object('id', interests.id, 'name', interests.name)
+			)
+		) AS interests
 		FROM users
-		JOIN users_interests ON users.id = users_interests.user_id
-		JOIN interests ON users_interests.interest_id = interests.id
-		WHERE id=$1`,
+		LEFT JOIN users_interests ON users.id = users_interests.user_id
+		LEFT JOIN interests ON users_interests.interest_id = interests.id
+		WHERE users.id=$1
+		GROUP BY users.id, users.name, users.bio, users.profession`,
 		id,
 	).Scan(&userInfo.Id, &userInfo.Name, &userInfo.Bio, &userInfo.Profession, &interestsJSON)
 
 	if err != nil {
+		println(err.Error())
 		return UserInfo{}, err
 	}
 
 	err = json.Unmarshal(interestsJSON, &userInfo.Interests)
 	if err != nil {
+		println(err.Error())
 		return UserInfo{}, err
 	}
 
@@ -53,7 +64,7 @@ func (um *UserModel) SaveInfo(userID, profession, bio string, interests []Intere
 		return err
 	}
 
-	_, err = tx.Exec("DELETE FROM users_interests WHERE id=$1", userID)
+	_, err = tx.Exec("DELETE FROM users_interests WHERE user_id=$1", userID)
 	if err != nil {
 		return err
 	}
