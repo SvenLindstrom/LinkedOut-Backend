80f9e0fa25fa6d07e566475b5352251e3f3f6d38
Bianca Casetta <biancacasetta@gmail.com>
2025-10-27 10:08:33 +0100

    changed userinfo struct

--- services/user/types.go
+++ services/user/types.go
@@ -1,10 +1,11 @@
 package user
 
 type UserInfo struct {
-	Id        string     `json:"id"`
-	Name      string     `json:"name"`
-	Bio       string     `json:"bio"`
-	Interests []Interest `json:"interests"`
+	Id         string     `json:"id"`
+	Name       string     `json:"name"`
+	Profession string     `json:"profession"`
+	Bio        string     `json:"bio"`
+	Interests  []Interest `json:"interests"`
 }
 
 type Interest struct {
