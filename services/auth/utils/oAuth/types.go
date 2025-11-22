666f9ec563ef529e43c954260a34235157adc678
SvenLindstrom <svenarne.Lindstroem@gmail.com>
2025-11-22 09:30:21 +0100

    refactored oauth code exchange

---
+++ services/auth/utils/oAuth/types.go
@@ -0,0 +1,27 @@
+package oauth
+
+import (
+	"io"
+
+	"golang.org/x/oauth2"
+)
+
+var getConfig = map[string]OAuthConfig{
+	"google":   getGoogleConf(),
+	"linkedin": getLinkedinConf(),
+}
+
+type OAuthConfig struct {
+	Config   *oauth2.Config
+	Endpoint string
+	Res      OAuthRes
+}
+
+type OAuthRes interface {
+	ToUserInfo(body io.Reader) UserInfo
+}
+
+type UserInfo struct {
+	Id   string
+	Name string
+}
